e6b1d573b89d3f3c2e81700e2b0eb87b94c8f140
Marshall Acharibia <macharibia@npontu.com>
2025-03-16 03:13:42 +0000

    update

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,7 +1,7 @@
 export default function AppLogo() {
     return (
         <>
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-full">
+            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
                 R
             </div>
         </>

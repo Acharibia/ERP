b8852997299218d9d78ce2c51646fab49e3497a9
Marshall Acharibia <macharibia@npontu.com>
2025-03-16 02:12:54 +0000

    update

--- app/Admin/Http/Controllers/DashboardController.php
+++ app/Admin/Http/Controllers/DashboardController.php
@@ -14,6 +14,6 @@ class DashboardController extends Controller
      */
     public function index(Request $request): Response
     {
-        return Inertia::render('admin/dashboard');
+        return Inertia::render('dashboard');
     }
 }
